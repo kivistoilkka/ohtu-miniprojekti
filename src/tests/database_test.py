d2d65340d1cd44175e9a84e9d2729b49de8cd3d7
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-24 16:29:31 +0200

    Lisätty ensimmäiset testit

---
+++ src/tests/database_test.py
@@ -0,0 +1,22 @@
+import os
+import sqlite3
+import unittest
+from database import get_database_connection, drop_tables, create_tables, initialize_database
+from repositories.book_reference_repo import BookReference
+
+class TestDatabase(unittest.TestCase):
+    def setUp(self):
+        dirname = os.path.dirname(__file__)
+        self.connection = sqlite3.connect(os.path.join(dirname, "testi.db"))
+        self.book_reference = BookReference
+
+    def test_initialize_database_creates_new_tables(self):
+        initialize_database(self.connection)
+        cursor = self.connection.cursor()
+
+        data = cursor.execute("SELECT id, author, title, year, publisher, bib_key FROM bookreferences").fetchall()
+        #self.assertEqual(self.book_reference.get_data(self.connection), [])
+        self.assertEqual(data, [])
+
+    
+
