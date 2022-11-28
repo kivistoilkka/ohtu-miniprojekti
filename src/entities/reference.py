2330f9e1699ded80aca36daa56b14430d032eed5
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-11-28 15:35:34 +0200

    reference-olio luotu

---
+++ src/entities/reference.py
@@ -0,0 +1,27 @@
+class Reference:
+    def __init__(self, author:str, title:str, year:int, publisher:str, bib_key:str):
+        self.__author = author
+        self.__title = title
+        self.__year = year
+        self.__publisher = publisher
+        self.__bib_key = bib_key
+
+    @property
+    def author(self):
+        return self.__author
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def year(self):
+        return self.__year
+
+    @property
+    def publisher(self):
+        return self.__publisher
+
+    @property
+    def bib_key(self):
+        return self.__bib_key
