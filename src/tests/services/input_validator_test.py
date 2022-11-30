f1fc4ffee83455a3d04a38e16b5101875fb43859
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-30 15:12:06 +0200

    Lisätty testejä

---
+++ src/tests/services/input_validator_test.py
@@ -0,0 +1,10 @@
+import unittest
+from services.input_validator_service import InputValidator
+
+class TestInputValidator(unittest.TestCase):
+    def setUp(self):
+        self.validator = InputValidator()
+
+    def test_validator_raises_exception_if_some_of_the_fields_is_empty(self):
+        ref_list = ["", "Test it to the limit", 2022, "TestPublishing", "test22"]
+        self.assertRaises(ValueError, self.validator.validate, ref_list)
