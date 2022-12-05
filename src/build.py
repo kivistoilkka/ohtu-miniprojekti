9ce9b267f5e078c2d2a3e9626347baaf06c2aae6
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-12-05 14:54:59 +0200

    Databasen buildauksen skripti korjattu

--- src/build.py
+++ src/build.py
@@ -2,8 +2,7 @@ from database import Database
 
 def build():
     database = Database()
-    connection = database.get_database_connection()
-    database.initialize_database(connection)
+    database.initialize_database()
 
 if __name__ == "__main__":
     build()
