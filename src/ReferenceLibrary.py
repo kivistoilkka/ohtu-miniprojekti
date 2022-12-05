c013efca530cdd2f0dd058b1974e367c0f06ab66
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-12-05 14:46:54 +0200

    Robottitestit korjattu bibtex-generaattorin lisäyksen ja databasen refaktoroinnin jälkeen

--- src/ReferenceLibrary.py
+++ src/ReferenceLibrary.py
@@ -6,13 +6,22 @@ from app import App
 from ui.stub_ui import StubUI
 from services.reference_service import ReferenceService
 from services.input_validator_service import InputValidator
+from services.bibtex_generator_service import BibtexGeneratorService
 from ui.reference_reader import ReferenceReader
 
 
 
 class ReferenceLibrary:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.db = Database(testing_environment=True)
+        self.book_reference = BookReference(self.db.get_database_connection())
+        validator = InputValidator()
+        self.reference_service = ReferenceService(self.book_reference, validator)
+        self.reference_reader = ReferenceReader()
+        self.bibtex_generator = BibtexGeneratorService()
+        app = App(self.db.get_database_connection(), self.book_reference,
+            self.db, self.reference_reader, self.bibtex_generator)
+        self.ui = StubUI(app, self.reference_service)
 
     def add_reference(self, author, title, year, publ, key):
         #keywordilla Add Reference Values voi syöttää tälle metodille
@@ -20,18 +29,18 @@ class ReferenceLibrary:
         self.ui.add_ref([author, title, year, publ, key])
 
 
-    def run(self):
-        dirname = os.path.dirname(__file__)
-        self.db = Database()
-        self.connection = sqlite3.connect(
-            os.path.join(dirname, "tests/robot_testi.db"))
-        self.db.initialize_database(self.connection)
-        self.book_reference = BookReference(self.connection)
-        self.reference_service = ReferenceService(self.book_reference, InputValidator())
-        self.reference_reader = ReferenceReader()
-        app = App(self.connection, self.book_reference,
-                  self.db, self.reference_reader)
-        self.ui = StubUI(app, self.reference_service)
+    # def run(self):
+    #     # dirname = os.path.dirname(__file__)
+    #     # self.db = Database()
+    #     # self.connection = sqlite3.connect(
+    #     #     os.path.join(dirname, "tests/robot_testi.db"))
+    #     # self.db.initialize_database(self.connection)
+    #     # self.book_reference = BookReference(self.connection)
+    #     # self.reference_service = ReferenceService(self.book_reference, InputValidator())
+    #     self.reference_reader = ReferenceReader()
+    #     app = App(self.connection, self.book_reference,
+    #               self.db, self.reference_reader)
+    #     self.ui = StubUI(app, self.reference_service)
 
     def data_in_database_length_should_be(self, length):
         #Logiikka tässä on, että voimme tarkistaa, onko tietokannassa nyt haluttu
