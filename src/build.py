3468015eef2385cd458ddf512319cc1fcb77a067
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-12-05 14:56:54 +0200

    Viesti käyttäjälle, kun uusi tietokanta on luotu

--- src/build.py
+++ src/build.py
@@ -3,6 +3,7 @@ from database import Database
 def build():
     database = Database()
     database.initialize_database()
+    print("New database created!")
 
 if __name__ == "__main__":
     build()
