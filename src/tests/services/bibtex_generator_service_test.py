7992d1990ab15b5a05779c459eb7e5d52a4b4f71
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-12 14:58:59 +0200

    Lisätty verkkosivuviitteet luotavaan bibtex-tiedostoon

--- src/tests/services/bibtex_generator_service_test.py
+++ src/tests/services/bibtex_generator_service_test.py
@@ -13,11 +13,11 @@ class TestBookReference(unittest.TestCase):
         self.bibtex_generator = BibtexGeneratorService()
 
     def test_create_bibtex_file_creates_correct_file(self):
-        refs = {'bookreferences': [BookReference(
+        refs = {'book_references': [BookReference(
             "Yrjänä Änkyräinen", "Test it to the limit", 2022, "TestPublishing", "test22", "")]}
         self.bibtex_generator.create_bibtex_file(refs, "test.bib")
 
-        correct_file = """@Book{test22,\n  author     = "Yrj{\\"a}n{\\"a} {\\"A}nkyr{\\"a}inen",\n  title      = "Test it to the limit",\n  publisher  = "TestPublishing",\n  year       = 2022\n}\n\n"""
+        correct_file = """@book{test22,\n  author     = "Yrj{\\"a}n{\\"a} {\\"A}nkyr{\\"a}inen",\n  title      = "Test it to the limit",\n  publisher  = "TestPublishing",\n  year       = 2022\n}\n\n"""
 
         self.assertEqual(self.TEST_FILE.open().read(), correct_file)
 
