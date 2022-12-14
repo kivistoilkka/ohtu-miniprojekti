0a23a1523d51d6c10e81f64951ff7b16725d7bd5
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-12-14 16:11:39 +0200

    Viitteiden poiston metodi yksinkertaisemmaksi

--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -76,9 +76,9 @@ class ReferenceService:
 
     def delete_reference(self, bib_key, ref_type) -> bool:
         if ref_type == ReferenceType.Book:
-            return True if self.book_repo.delete_from_table(bib_key) else False
+            return self.book_repo.delete_from_table(bib_key)
         if ref_type == ReferenceType.Website:
-            return True if self.web_repo.delete_from_table(bib_key) else False
+            return self.web_repo.delete_from_table(bib_key)
         return False
 
     def get_reference(self, bib_key, ref_type):
